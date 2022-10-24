499409c776eb18cd2dc367ed8f56e8d1f65565ae
NRHerman <nathanaelherman@gmail.com>
2022-10-24 14:17:25 -0400

    Video background template and logo template half-finished.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <video autoplay loop muted class="video">
+    <source src="./assets/BeeBackground.mp4" type="video/webm">
+    <source src="./assets/BeeBackground.mp4" type="video/mp4">
+  </video>
+  <img alt="Vue logo" src="./assets/logo.png">
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -22,5 +27,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img {
+  margin: 120px;
+  width: 7.5%;
+  margin-top: -50px;
+}
+
+video {
+  z-index: -1000;
+  left: 50%;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
 }
 </style>
