69e28170dc0a4b47506ca57b2eefea6764949d29
NRHerman <nathanaelherman@gmail.com>
2022-10-24 15:52:03 -0400

    Video Background Test Complete

--- src/App.vue
+++ src/App.vue
@@ -43,5 +43,9 @@ video {
   position: absolute;
   top: 50%;
   -webkit-transform: translate(-50%, -50%);
+  -moz-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  object-fit: cover;
 }
 </style>
