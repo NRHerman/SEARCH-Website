3d418a61caf8cd8b79cffe4030687d3b5980ac11
NRHerman <nathanaelherman@gmail.com>
2023-01-13 15:39:42 -0500

    Saving

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <head>
+    <title>
+      Space and Earth Analogs Research Chapter of Purdue
+    </title>
+    <meta name="description" content="We are Purdue University's premiere space focused chapter. Our vision as a multidisciplinary organization of students, faculty, staff, and alumni is to add more members and partners to the Cradle of Astronauts as well as increase support for human spaceflight and space exploration.">
+  </head>
+
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;605&display=swap" rel="stylesheet">
@@ -101,9 +108,7 @@
 import { SidebarMenu } from 'vue-sidebar-menu'
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 import ImageToText from "./components/ImageToText"
-/*import Vue from 'vue'
-import VueMeta from 'vue-meta'
-Vue.use(VueMeta)*/
+//import VueMeta from 'vue-meta'
 
 export default {
   name: 'App',
